c2f4b43b0e1deceaf2b96aa914322e81954ad7b2
Szabolcs Fruhwald <szabolcs_fruhwald@intuit.com>
2021-05-28 10:18:27 -0700

    test: add more tests

--- internal/env/env_test.go
+++ internal/env/env_test.go
@@ -15,7 +15,7 @@ func TestGetEnv(t *testing.T) {
 	os.Setenv("VAULT_ADDR", "1.2.3.4")
 	os.Setenv("ARGO_ADDR", "2.3.4.5")
 	os.Setenv("ARGO_CLOUDOPS_NAMESPACE", "argo-ns")
-	os.Setenv("CONFIG", "/app/test/config/path")
+	os.Setenv("ARGO_CLOUDOPS_CONFIG", "/app/test/config/path")
 	os.Setenv("SSH_PEM_FILE", "/app/test/ssh.pem")
 	os.Setenv("ARGO_CLOUDOPS_LOG_LEVEL", "DEBUG")
 	os.Setenv("ARGO_CLOUDOPS_PORT", "1234")
@@ -53,32 +53,47 @@ func TestGetEnv(t *testing.T) {
 	}
 }
 
-// type EnvVars struct {
-// 	AdminSecret    string `split_words:"true"`
-// 	VaultRole      string `envconfig:"VAULT_ROLE" required:"true"`
-// 	VaultSecret    string `envconfig:"VAULT_SECRET" required:"true"`
-// 	VaultAddress   string `envconfig:"VAULT_ADDR" required:"true"`
-// 	ArgoAddress    string `envconfig:"ARGO_ADDR" required:"true"`
-// 	Namespace      string `default:"argo"`
-// 	ConfigFilePath string `envconfig:"CONFIG" default:"argo-cloudops.yaml"`
-// 	SshPemFile     string `envconfig:"SSH_PEM_FILE" required:"true"`
-// 	LogLevel       string `split_words:"true"`
-// 	Port           int32
+func TestDefaults(t *testing.T) {
+	os.Clearenv()
+	os.Setenv("ARGO_CLOUDOPS_ADMIN_SECRET", testSecret)
+	os.Setenv("VAULT_ROLE", "vaultRole")
+	os.Setenv("VAULT_SECRET", testSecret)
+	os.Setenv("VAULT_ADDR", "1.2.3.4")
+	os.Setenv("ARGO_ADDR", "2.3.4.5")
+	os.Setenv("SSH_PEM_FILE", "/app/test/ssh.pem")
+	var env EnvVars = GetEnv()
+	if env.Namespace != "argo" {
+		t.Errorf("expected %v, got %v", "argo-ns", env.Namespace)
+	}
+	if env.ConfigFilePath != "argo-cloudops.yaml" {
+		t.Errorf("expected %v, got %v", "/app/test/config/path", env.ConfigFilePath)
+	}
+	if env.Port != 8443 {
+		t.Errorf("expected %v, got %v", 8443, env.Port)
+	}
+}
+
+// func TestRequiredVars(t *testing.T) {
+// 	os.Clearenv()
+// 	//os.Setenv("ARGO_CLOUDOPS_ADMIN_SECRET", testSecret)
+// 	os.Setenv("VAULT_ROLE", "vaultRole")
+// 	os.Setenv("VAULT_SECRET", testSecret)
+// 	os.Setenv("VAULT_ADDR", "1.2.3.4")
+// 	os.Setenv("ARGO_ADDR", "2.3.4.5")
+// 	os.Setenv("ARGO_CLOUDOPS_NAMESPACE", "argo-ns")
+// 	os.Setenv("ARGO_CLOUDOPS_CONFIG", "/app/test/config/path")
+// 	os.Setenv("SSH_PEM_FILE", "/app/test/ssh.pem")
+// 	os.Setenv("ARGO_CLOUDOPS_LOG_LEVEL", "DEBUG")
+// 	os.Setenv("ARGO_CLOUDOPS_PORT", "1234")
+// 	//assertPanic(t, env.GetEnv())
+// 	GetEnv()
 // }
 
-func TestDefaults(t *testing.T) {
-	// TODO
-}
-
-func TestRequiredVars(t *testing.T) {
-	// TODO
-}
-
-func assertPanic(t *testing.T, f func()) {
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("The code did not panic")
-		}
-	}()
-	f()
-}
+// func assertPanic(t *testing.T, f func()) {
+// 	defer func() {
+// 		if r := recover(); r == nil {
+// 			t.Errorf("The code did not panic")
+// 		}
+// 	}()
+// 	f()
+// }
