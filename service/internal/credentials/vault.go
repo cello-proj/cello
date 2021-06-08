44f8accd6acef12d2aac970216f7c07d630df2b2
grantleehoffman <grantleehoffman [at] gmail.com>
2021-06-08 12:19:48 -0700

    add more tests, pr review changes

--- service/internal/credentials/vault.go
+++ service/internal/credentials/vault.go
@@ -9,28 +9,28 @@ import (
 )
 
 type Provider interface {
-	GetToken() (string, error)
 	CreateProject(string) (string, string, error)
-	GetProject(string) (string, error)
-	DeleteProject(string) error
 	CreateTarget(string, CreateTargetRequest) error
+	DeleteProject(string) error
 	DeleteTarget(string, string) error
+	GetProject(string) (string, error)
 	GetTarget(string, string) (TargetProperties, error)
+	GetToken() (string, error)
 	ListTargets(string) ([]string, error)
 	ProjectExists(string) (bool, error)
 	TargetExists(name string) (bool, error)
 }
 
 type VaultLogical interface {
-	Read(path string) (*vault.Secret, error)
-	List(path string) (*vault.Secret, error)
-	Write(path string, data map[string]interface{}) (*vault.Secret, error)
 	Delete(path string) (*vault.Secret, error)
+	List(path string) (*vault.Secret, error)
+	Read(path string) (*vault.Secret, error)
+	Write(path string, data map[string]interface{}) (*vault.Secret, error)
 }
 
 type VaultSys interface {
-	PutPolicy(name, rules string) error
 	DeletePolicy(name string) error
+	PutPolicy(name, rules string) error
 }
 
 // Vault
@@ -44,10 +44,10 @@ var (
 )
 
 type VaultProvider struct {
-	VaultLogicalSvc VaultLogical
-	VaultSysSvc     VaultSys
 	RoleID          string
 	SecretID        string
+	VaultLogicalSvc VaultLogical
+	VaultSysSvc     VaultSys
 }
 
 type TargetProperties struct {
@@ -58,148 +58,20 @@ type TargetProperties struct {
 
 type CreateTargetRequest struct {
 	Name       string           `json:"name"`
-	Type       string           `json:"type"`
 	Properties TargetProperties `json:"properties"`
+	Type       string           `json:"type"`
 }
 
 type CreateProjectRequest struct {
 	Name string `json:"name"`
 }
 
-func genProjectAppRole(name string) string {
-	return fmt.Sprintf("%s/%s-%s", vaultAppRolePrefix, vaultProjectPrefix, name)
-}
-
-func (v VaultProvider) ProjectExists(name string) (bool, error) {
-	p, err := v.GetProject(name)
-	if errors.Is(err, ErrNotFound) {
-		return false, nil
-	}
-
-	if err != nil {
-		return false, err
-	}
-
-	return p != "", nil
-}
-
-func (v VaultProvider) TargetExists(name string) (bool, error) {
-	// TODO: Implement targetExists call
-	return false, nil
-}
-
-func (v VaultProvider) isAdmin() bool {
-	return v.RoleID == "admin"
-}
-
-func (v VaultProvider) GetToken() (string, error) {
-	if v.isAdmin() {
-		return "", errors.New("admin credentials cannot be used to get tokens")
-	}
-
-	options := map[string]interface{}{
-		"role_id":   v.RoleID,
-		"secret_id": v.SecretID,
-	}
-
-	sec, err := v.VaultLogicalSvc.Write("auth/approle/login", options)
-	if err != nil {
-		fmt.Println(err.Error())
-		return "", err
-	}
-
-	return sec.Auth.ClientToken, nil
-}
-
 func (v VaultProvider) createPolicyState(name, policy string) error {
 	return v.VaultSysSvc.PutPolicy(fmt.Sprintf("%s-%s", vaultProjectPrefix, name), policy)
 }
 
-func (v VaultProvider) deletePolicyState(name string) error {
-	return v.VaultSysSvc.DeletePolicy(fmt.Sprintf("%s-%s", vaultProjectPrefix, name))
-}
-
-// TODO validate policy and other information is correct in target
-// Validate role exists (if possible, etc)
-func (v VaultProvider) CreateTarget(projectName string, ctr CreateTargetRequest) error {
-	if !v.isAdmin() {
-		return errors.New("admin credentials must be used to create target")
-	}
-
-	targetName := ctr.Name
-	credentialType := ctr.Properties.CredentialType
-	policyArns := ctr.Properties.PolicyArns
-	roleArn := ctr.Properties.RoleArn
-
-	options := map[string]interface{}{
-		"role_arns":       roleArn,
-		"credential_type": credentialType,
-		"policy_arns":     policyArns,
-	}
-
-	path := fmt.Sprintf("aws/roles/%s-%s-target-%s", vaultProjectPrefix, projectName, targetName)
-	_, err := v.VaultLogicalSvc.Write(path, options)
-	return err
-}
-
-func (v VaultProvider) DeleteTarget(projectName string, targetName string) error {
-	if !v.isAdmin() {
-		return errors.New("admin credentials must be used to delete target")
-	}
-
-	path := fmt.Sprintf("aws/roles/%s-%s-target-%s", vaultProjectPrefix, projectName, targetName)
-	_, err := v.VaultLogicalSvc.Delete(path)
-	return err
-}
-
-const (
-	vaultSecretTTL   = "8776h" // 1 year
-	vaultTokenMaxTTL = "10m"
-	// When set to 1 with the cli or api, it will not return the creds as it
-	// says it's hit the limit of uses.
-	vaultTokenNumUses = 3
-)
-
-func (v VaultProvider) writeProjectState(name string) error {
-	options := map[string]interface{}{
-		"secret_id_ttl":           vaultSecretTTL,
-		"token_max_ttl":           vaultTokenMaxTTL,
-		"token_no_default_policy": "true",
-		"token_num_uses":          vaultTokenNumUses,
-		"token_policies":          fmt.Sprintf("%s-%s", vaultProjectPrefix, name),
-	}
-
-	_, err := v.VaultLogicalSvc.Write(genProjectAppRole(name), options)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (v VaultProvider) readSecretID(appRoleName string) (string, error) {
-	options := map[string]interface{}{
-		"force": true,
-	}
-	secret, err := v.VaultLogicalSvc.Write(fmt.Sprintf("%s/secret-id", genProjectAppRole(appRoleName)), options)
-	if err != nil {
-		return "", err
-	}
-	return secret.Data["secret_id"].(string), nil
-}
-
-func (v VaultProvider) readRoleID(appRoleName string) (string, error) {
-	secret, err := v.VaultLogicalSvc.Read(fmt.Sprintf("%s/role-id", genProjectAppRole(appRoleName)))
-	if err != nil {
-		return "", err
-	}
-	return secret.Data["role_id"].(string), nil
-}
-
-func defaultVaultReadonlyPolicyAWS(projectName string) string {
-	return fmt.Sprintf(
-		"path \"aws/sts/argo-cloudops-projects-%s-target-*\" { capabilities = [\"read\"] }",
-		projectName,
-	)
+func genProjectAppRole(name string) string {
+	return fmt.Sprintf("%s/%s-%s", vaultAppRolePrefix, vaultProjectPrefix, name)
 }
 
 func (v VaultProvider) CreateProject(name string) (string, string, error) {
@@ -231,39 +103,38 @@ func (v VaultProvider) CreateProject(name string) (string, string, error) {
 	return roleID, secretID, nil
 }
 
-func (v VaultProvider) ListTargets(project string) ([]string, error) {
+// TODO validate policy and other information is correct in target
+// Validate role exists (if possible, etc)
+func (v VaultProvider) CreateTarget(projectName string, ctr CreateTargetRequest) error {
 	if !v.isAdmin() {
-		return nil, errors.New("admin credentials must be used to list targets")
+		return errors.New("admin credentials must be used to create target")
 	}
 
-	sec, err := v.VaultLogicalSvc.List("aws/roles/")
-	if err != nil {
-		return nil, fmt.Errorf("vault list error: %v", err)
+	targetName := ctr.Name
+	credentialType := ctr.Properties.CredentialType
+	policyArns := ctr.Properties.PolicyArns
+	roleArn := ctr.Properties.RoleArn
+
+	options := map[string]interface{}{
+		"role_arns":       roleArn,
+		"credential_type": credentialType,
+		"policy_arns":     policyArns,
 	}
 
-	// allow empty array to render json as []
-	list := make([]string, 0)
-	if sec != nil {
-		for _, target := range sec.Data["keys"].([]interface{}) {
-			value := target.(string)
-			if strings.HasPrefix(value, fmt.Sprintf("argo-cloudops-projects-%s-target-", project)) {
-				list = append(list, value)
-			}
-		}
-	}
-
-	return list, nil
+	path := fmt.Sprintf("aws/roles/%s-%s-target-%s", vaultProjectPrefix, projectName, targetName)
+	_, err := v.VaultLogicalSvc.Write(path, options)
+	return err
 }
 
-func (v VaultProvider) GetProject(projectName string) (string, error) {
-	sec, err := v.VaultLogicalSvc.Read(genProjectAppRole(projectName))
-	if err != nil {
-		return "", fmt.Errorf("vault get project error: %v", err)
-	}
-	if sec == nil {
-		return "", ErrNotFound
-	}
-	return fmt.Sprintf(`{"name":"%s"}`, projectName), nil
+func defaultVaultReadonlyPolicyAWS(projectName string) string {
+	return fmt.Sprintf(
+		"path \"aws/sts/argo-cloudops-projects-%s-target-*\" { capabilities = [\"read\"] }",
+		projectName,
+	)
+}
+
+func (v VaultProvider) deletePolicyState(name string) error {
+	return v.VaultSysSvc.DeletePolicy(fmt.Sprintf("%s-%s", vaultProjectPrefix, name))
 }
 
 func (v VaultProvider) DeleteProject(name string) error {
@@ -281,6 +152,35 @@ func (v VaultProvider) DeleteProject(name string) error {
 		return fmt.Errorf("vault delete project error: %v", err)
 	}
 	return nil
+}
+
+func (v VaultProvider) DeleteTarget(projectName string, targetName string) error {
+	if !v.isAdmin() {
+		return errors.New("admin credentials must be used to delete target")
+	}
+
+	path := fmt.Sprintf("aws/roles/%s-%s-target-%s", vaultProjectPrefix, projectName, targetName)
+	_, err := v.VaultLogicalSvc.Delete(path)
+	return err
+}
+
+const (
+	vaultSecretTTL   = "8776h" // 1 year
+	vaultTokenMaxTTL = "10m"
+	// When set to 1 with the cli or api, it will not return the creds as it
+	// says it's hit the limit of uses.
+	vaultTokenNumUses = 3
+)
+
+func (v VaultProvider) GetProject(projectName string) (string, error) {
+	sec, err := v.VaultLogicalSvc.Read(genProjectAppRole(projectName))
+	if err != nil {
+		return "", fmt.Errorf("vault get project error: %v", err)
+	}
+	if sec == nil {
+		return "", ErrNotFound
+	}
+	return fmt.Sprintf(`{"name":"%s"}`, projectName), nil
 }
 
 func (v VaultProvider) GetTarget(projectName, targetName string) (TargetProperties, error) {
@@ -307,4 +207,105 @@ func (v VaultProvider) GetTarget(projectName, targetName string) (TargetProperti
 	}
 
 	return TargetProperties{CredentialType: credentialType, RoleArn: roleArn, PolicyArns: policies}, nil
+}
+
+func (v VaultProvider) GetToken() (string, error) {
+	if v.isAdmin() {
+		return "", errors.New("admin credentials cannot be used to get tokens")
+	}
+
+	options := map[string]interface{}{
+		"role_id":   v.RoleID,
+		"secret_id": v.SecretID,
+	}
+
+	sec, err := v.VaultLogicalSvc.Write("auth/approle/login", options)
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	return sec.Auth.ClientToken, nil
+}
+
+func (v VaultProvider) isAdmin() bool {
+	return v.RoleID == "admin"
+}
+
+func (v VaultProvider) ListTargets(project string) ([]string, error) {
+	if !v.isAdmin() {
+		return nil, errors.New("admin credentials must be used to list targets")
+	}
+
+	sec, err := v.VaultLogicalSvc.List("aws/roles/")
+	if err != nil {
+		return nil, fmt.Errorf("vault list error: %v", err)
+	}
+
+	// allow empty array to render json as []
+	list := make([]string, 0)
+	if sec != nil {
+		for _, target := range sec.Data["keys"].([]interface{}) {
+			value := target.(string)
+			prefix := fmt.Sprintf("argo-cloudops-projects-%s-target-", project)
+			if strings.HasPrefix(value, prefix) {
+				list = append(list, strings.Replace(value, prefix, "", 1))
+			}
+		}
+	}
+
+	return list, nil
+}
+
+func (v VaultProvider) ProjectExists(name string) (bool, error) {
+	p, err := v.GetProject(name)
+	if errors.Is(err, ErrNotFound) {
+		return false, nil
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	return p != "", nil
+}
+
+func (v VaultProvider) readRoleID(appRoleName string) (string, error) {
+	secret, err := v.VaultLogicalSvc.Read(fmt.Sprintf("%s/role-id", genProjectAppRole(appRoleName)))
+	if err != nil {
+		return "", err
+	}
+	return secret.Data["role_id"].(string), nil
+}
+
+func (v VaultProvider) readSecretID(appRoleName string) (string, error) {
+	options := map[string]interface{}{
+		"force": true,
+	}
+	secret, err := v.VaultLogicalSvc.Write(fmt.Sprintf("%s/secret-id", genProjectAppRole(appRoleName)), options)
+	if err != nil {
+		return "", err
+	}
+	return secret.Data["secret_id"].(string), nil
+}
+
+func (v VaultProvider) TargetExists(name string) (bool, error) {
+	// TODO: Implement targetExists call
+	return false, nil
+}
+
+func (v VaultProvider) writeProjectState(name string) error {
+	options := map[string]interface{}{
+		"secret_id_ttl":           vaultSecretTTL,
+		"token_max_ttl":           vaultTokenMaxTTL,
+		"token_no_default_policy": "true",
+		"token_num_uses":          vaultTokenNumUses,
+		"token_policies":          fmt.Sprintf("%s-%s", vaultProjectPrefix, name),
+	}
+
+	_, err := v.VaultLogicalSvc.Write(genProjectAppRole(name), options)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- service/internal/credentials/vault_test.go
+++ service/internal/credentials/vault_test.go
@@ -8,26 +8,426 @@ import (
 	vault "github.com/hashicorp/vault/api"
 )
 
+var testErr = fmt.Errorf("error")
+
+func TestVaultCreateProject(t *testing.T) {
+	tests := []struct {
+		name           string
+		admin          bool
+		expectedRole   string
+		expectedSecret string
+		vaultErr       error
+		errResult      bool
+	}{
+		{
+			name:           "create project success",
+			admin:          true,
+			expectedSecret: "test-secret",
+			expectedRole:   "test-role",
+		},
+		{
+			name:      "create project admin error",
+			admin:     false,
+			errResult: true,
+		},
+		{
+			name:      "create prolicy error",
+			admin:     true,
+			vaultErr:  testErr,
+			errResult: true,
+		},
+		{
+			name:      "create project error",
+			admin:     true,
+			vaultErr:  testErr,
+			errResult: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var role = "testRole"
+			if tt.admin == true {
+				role = "admin"
+			}
+			v := VaultProvider{
+				RoleID: role,
+				VaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr, data: map[string]interface{}{
+					"secret_id": tt.expectedSecret,
+					"role_id":   tt.expectedRole,
+				}},
+				VaultSysSvc: &mockVaultSys{},
+			}
+
+			roleID, secretID, err := v.CreateProject("testProject")
+			if err != nil {
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
+				}
+				if !cmp.Equal(roleID, tt.expectedRole) {
+					t.Errorf("\nwant: %v\n got: %v", tt.expectedRole, roleID)
+				}
+				if !cmp.Equal(secretID, tt.expectedSecret) {
+					t.Errorf("\nwant: %v\n got: %v", tt.expectedSecret, secretID)
+				}
+			}
+
+		})
+	}
+}
+
+func TestVaultCreateTarget(t *testing.T) {
+	tests := []struct {
+		name      string
+		admin     bool
+		vaultErr  error
+		errResult bool
+	}{
+		{
+			name:  "create target success",
+			admin: true,
+		},
+		{
+			name:      "create target admin error",
+			admin:     false,
+			errResult: true,
+		},
+		{
+			name:      "create target error",
+			admin:     true,
+			vaultErr:  testErr,
+			errResult: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var role = "testRole"
+			if tt.admin == true {
+				role = "admin"
+			}
+			v := VaultProvider{
+				RoleID:          role,
+				VaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr},
+			}
+
+			err := v.CreateTarget("test", CreateTargetRequest{})
+			if err != nil {
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
+				}
+			}
+		})
+	}
+}
+
+func TestVaultDeleteProject(t *testing.T) {
+	tests := []struct {
+		name           string
+		admin          bool
+		vaultErr       error
+		vaultPolicyErr error
+		errResult      bool
+	}{
+		{
+			name:  "delete project success",
+			admin: true,
+		},
+		{
+			name:      "delete project admin error",
+			admin:     false,
+			errResult: true,
+		},
+		{
+			name:      "delete project error",
+			admin:     true,
+			vaultErr:  testErr,
+			errResult: true,
+		},
+		{
+			name:           "delete project policy error",
+			admin:          true,
+			vaultPolicyErr: testErr,
+			errResult:      true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var role = "testRole"
+			if tt.admin == true {
+				role = "admin"
+			}
+			v := VaultProvider{
+				RoleID:          role,
+				VaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr},
+				VaultSysSvc:     &mockVaultSys{err: tt.vaultPolicyErr},
+			}
+
+			err := v.DeleteProject("testProject")
+			if err != nil {
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
+				}
+			}
+		})
+	}
+}
+
+func TestVaultDeleteTarget(t *testing.T) {
+	tests := []struct {
+		name      string
+		admin     bool
+		vaultErr  error
+		errResult bool
+	}{
+		{
+			name:  "delete target success",
+			admin: true,
+		},
+		{
+			name:      "delete target admin error",
+			admin:     false,
+			errResult: true,
+		},
+		{
+			name:      "delete target error",
+			admin:     true,
+			vaultErr:  testErr,
+			errResult: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var role = "testRole"
+			if tt.admin == true {
+				role = "admin"
+			}
+			v := VaultProvider{
+				RoleID:          role,
+				VaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr},
+			}
+
+			err := v.DeleteTarget("testProject", "testTarget")
+			if err != nil {
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
+				}
+			}
+		})
+	}
+}
+
+func TestVaultGetTarget(t *testing.T) {
+	tests := []struct {
+		name               string
+		admin              bool
+		expectedProperties TargetProperties
+		vaultErr           error
+		errResult          bool
+	}{
+		{
+			name:  "get target success",
+			admin: true,
+		},
+		{
+			name:      "delete target admin error",
+			admin:     false,
+			errResult: true,
+		},
+		{
+			name:      "delete target error",
+			admin:     true,
+			vaultErr:  testErr,
+			errResult: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var role = "testRole"
+			if tt.admin == true {
+				role = "admin"
+			}
+			v := VaultProvider{
+				RoleID: role,
+				VaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr, data: map[string]interface{}{
+					"role_arns":       []interface{}{"test-role-arn"},
+					"policy_arns":     []interface{}{"test-policy-arn"},
+					"credential_type": "test-cred-type",
+				}},
+			}
+
+			_, err := v.GetTarget("testProject", "testTarget")
+			if err != nil {
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
+				}
+			}
+		})
+	}
+}
+
+func TestVaultGetToken(t *testing.T) {
+	tests := []struct {
+		name      string
+		token     string
+		admin     bool
+		vaultErr  error
+		errResult bool
+	}{
+		{
+			name:  "get token success",
+			token: "secretToken",
+		},
+		{
+			name:      "get token admin error",
+			admin:     true,
+			errResult: true,
+		},
+		{
+			name:      "get token error",
+			vaultErr:  testErr,
+			errResult: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var role = "testRole"
+			if tt.admin == true {
+				role = "admin"
+			}
+			v := VaultProvider{
+				RoleID:          role,
+				VaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr, token: tt.token},
+			}
+
+			token, err := v.GetToken()
+			if err != nil {
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
+				}
+				if !cmp.Equal(token, tt.token) {
+					t.Errorf("\nwant: %v\n got: %v", tt.token, token)
+				}
+			}
+		})
+	}
+}
+
+func TestVaultListTargets(t *testing.T) {
+	tests := []struct {
+		name            string
+		admin           bool
+		targets         []string
+		expectedTargets []string
+		vaultErr        error
+		errResult       bool
+	}{
+		{
+			name:    "list target success",
+			admin:   true,
+			targets: []string{"target1", "target2"},
+		},
+		{
+			name:      "list target admin error",
+			admin:     false,
+			errResult: true,
+		},
+		{
+			name:      "list target error",
+			admin:     true,
+			vaultErr:  testErr,
+			errResult: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var role = "testRole"
+			if tt.admin == true {
+				role = "admin"
+			}
+			var testTargets []interface{}
+			for _, i := range tt.targets {
+				testTargets = append(testTargets, fmt.Sprintf("argo-cloudops-projects-test-target-%s", i))
+			}
+			v := VaultProvider{
+				RoleID: role,
+				VaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr, data: map[string]interface{}{
+					"keys": testTargets,
+				}},
+			}
+
+			targets, err := v.ListTargets("test")
+			if err != nil {
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
+				}
+				if !cmp.Equal(targets, tt.targets) {
+					t.Errorf("\nwant: %v\n got: %v", tt.targets, targets)
+				}
+			}
+		})
+	}
+}
+
 func TestVaultProjectExists(t *testing.T) {
 	tests := []struct {
 		name      string
 		path      string
 		exists    bool
 		vaultErr  error
-		errResult error
+		errResult bool
 	}{
 		{
-			name:      "get project success",
-			path:      "test-path",
-			exists:    true,
-			errResult: nil,
+			name:   "get project success",
+			path:   "test-path",
+			exists: true,
 		},
 		{
 			name:      "get project not found",
 			path:      "test-path",
 			exists:    false,
 			vaultErr:  ErrNotFound,
-			errResult: fmt.Errorf("vault get project error: %v", ErrNotFound),
+			errResult: true,
 		},
 	}
 
@@ -40,21 +440,17 @@ func TestVaultProjectExists(t *testing.T) {
 
 			status, err := v.ProjectExists(tt.path)
 			if err != nil {
-				if tt.errResult != nil && tt.errResult.Error() != err.Error() {
-					t.Errorf("\nwant: %v\n got: %v", tt.errResult, err)
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
 				}
 
-				if tt.errResult == nil {
-					t.Errorf("\nwant: %v\n got: %v", tt.errResult, err)
+				if !cmp.Equal(status, tt.exists) {
+					t.Errorf("\nwant: %v\n got: %v", tt.exists, status)
 				}
-			}
-
-			if err == nil && tt.errResult != nil {
-				t.Errorf("\nwant: %v\n got: %v", tt.errResult, err)
-			}
-
-			if !cmp.Equal(status, tt.exists) {
-				t.Errorf("\nwant: %v\n got: %v", tt.exists, status)
 			}
 		})
 	}
@@ -62,12 +458,48 @@ func TestVaultProjectExists(t *testing.T) {
 
 type mockVaultLogical struct {
 	vault.Logical
-	err error
+	data  map[string]interface{}
+	token string
+	err   error
 }
 
 func (m mockVaultLogical) Read(path string) (*vault.Secret, error) {
 	if m.err != nil {
 		return nil, m.err
 	}
+	return &vault.Secret{Data: m.data}, nil
+}
+
+func (m mockVaultLogical) List(path string) (*vault.Secret, error) {
+	if m.err != nil {
+		return nil, m.err
+	}
+	return &vault.Secret{Data: m.data}, nil
+}
+
+func (m mockVaultLogical) Write(path string, data map[string]interface{}) (*vault.Secret, error) {
+	if m.err != nil {
+		return nil, m.err
+	}
+	return &vault.Secret{Data: m.data, Auth: &vault.SecretAuth{ClientToken: m.token}}, nil
+}
+
+func (m mockVaultLogical) Delete(path string) (*vault.Secret, error) {
+	if m.err != nil {
+		return nil, m.err
+	}
 	return &vault.Secret{}, nil
+}
+
+type mockVaultSys struct {
+	vault.Sys
+	err error
+}
+
+func (m mockVaultSys) PutPolicy(name, rules string) error {
+	return m.err
+}
+
+func (m mockVaultSys) DeletePolicy(name string) error {
+	return m.err
 }
