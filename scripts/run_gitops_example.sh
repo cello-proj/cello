1441e087615621ce05c02871cc581ea0236c0202
Ameen Radwan <ameen_radwan@intuit.com>
2021-07-14 10:59:50 -0700

    added script for running examples via gitops

---
+++ scripts/run_gitops_example.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+GIT_PATH=$1
+GIT_SHA=$2
+
+if [ -z $ARGO_CLOUDOPS_PROJECT_NAME ]; then
+    export ARGO_CLOUDOPS_PROJECT_NAME=project1
+fi
+
+if [ -z $ARGO_CLOUDOPS_TARGET_NAME ]; then
+    export ARGO_CLOUDOPS_TARGET_NAME=target1
+fi
+
+if [ -z $ARGO_CLOUDOPS_SERVICE_ADDR ]; then
+    # if not set assume local development
+    export ARGO_CLOUDOPS_SERVICE_ADDR=https://localhost:8443
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CLI=$SCRIPT_DIR/../build/argo-cloudops
+
+$CLI sync \
+    -n $ARGO_CLOUDOPS_PROJECT_NAME \
+    -t $ARGO_CLOUDOPS_TARGET_NAME \
+    -p $GIT_PATH \
+    -s $GIT_SHA
