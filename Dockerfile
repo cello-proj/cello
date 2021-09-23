750f67ca5fcfe778ffd4f53ee5ec13a90f385b8d
Tommy Bishop <thomas_bishop@intuit.com>
2021-09-23 13:13:10 -0700

    Fix 'quickstart'.
    * Fixes and simplifies quickstart binary downloading.
    * Fixes issue when the quickstart argo workflow template already exists.
    * Now supports 'credential_process' for AWS credentials.
    * Simplifies docker image building.
    * Reduces docker image build from ~6m to ~20s.
    * Updates quickstart copy.

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,12 @@
-####################################################################################################
-# Build binaries
-####################################################################################################
-FROM golang:1.16.6 as build
-
-WORKDIR /go/src/github.com/argoproj-labs/argo-cloudops
-
-COPY go.mod ./
-COPY go.sum ./
-
-RUN go mod download
-
-COPY internal ./internal
-COPY service ./service
-COPY cli ./cli
-COPY Makefile ./
-
-COPY .git ./.git
-
-RUN make
-
-####################################################################################################
-# Run image
-####################################################################################################
 FROM debian:latest
 
+ARG BINARY
+
 RUN apt-get -yq update && \
-    apt-get -yqq install ssh && \
-    apt-get -yqq install openssl && \
-    apt-get -yq install ca-certificates && \
+    apt-get -yq install ssh openssl ca-certificates && \
     rm -rf /var/lib/apt/lists/*
 
-COPY --from=build /go/src/github.com/argoproj-labs/argo-cloudops/build/service ./
-COPY argo-cloudops.yaml ./
+COPY $BINARY argo-cloudops.yaml ./
 
 EXPOSE 8443
 
--- scripts/quickstart_run.sh
+++ scripts/quickstart_run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ARGO_CLOUDOPS_VERSION=0.8.1
 ARGO_WORKFLOWS_VERSION=3.0.10
 
 set -e
@@ -53,9 +52,31 @@ if [ $? != 0 ]; then
 fi
 set -e
 
+# Download artifacts
+mkdir -p quickstart
+
+latest_release=$(curl --silent "https://api.github.com/repos/argoproj-labs/argo-cloudops/releases/latest" | jq -r .tag_name
+)
+# remove leading 'v'
+latest_release="${latest_release//v}"
+
+# download Argo Cloudops CLI if it doesn't exist
+if [ ! -f quickstart/argo-cloudops ]; then
+    curl -L https://github.com/argoproj-labs/argo-cloudops/releases/download/v${latest_release}/argo-cloudops_cli_${latest_release}_darwin_x86_64.tar.gz -o quickstart/argo-cloudops_cli_${latest_release}_darwin_x86_64.tar.gz &> /dev/null
+      tar -xzf quickstart/argo-cloudops_cli_${latest_release}_darwin_x86_64.tar.gz -C quickstart/ #&> /dev/null
+        rm quickstart/argo-cloudops_cli_${latest_release}_darwin_x86_64.tar.gz &> /dev/null
+fi
+
+# download Argo CloudOps service binary if it doesn't exist
+if [ ! -f quickstart/service ]; then
+  curl -L https://github.com/argoproj-labs/argo-cloudops/releases/download/v${latest_release}/argo-cloudops_service_${latest_release}_linux_x86_64.tar.gz -o quickstart/argo-cloudops_service_${latest_release}_linux_x86_64.tar.gz &> /dev/null
+  tar -xzf quickstart/argo-cloudops_service_${latest_release}_linux_x86_64.tar.gz -C quickstart/ &> /dev/null
+  rm quickstart/argo-cloudops_service_${latest_release}_linux_x86_64.tar.gz &> /dev/null
+fi
+
 set +e
 echo "Building docker image"
-docker build --pull --rm -f "Dockerfile" -t argocloudops:latest "."
+docker build --pull --rm -f "Dockerfile" --build-arg BINARY=quickstart/service -t argocloudops:latest "."
 echo "Applying manifest"
 kubectl apply -f ./scripts/quickstart_manifest.yaml
 # Sleeping after applying manifest so pods have time to start
@@ -87,16 +108,51 @@ fi
 set -e
 
 # setup workflow if it doesn't exist
-set -e
+set +e
 argo template get -n argo argo-cloudops-single-step-vault-aws &> /dev/null
 if [ $? != 0 ]; then
   argo template create -n argo workflows/argo-cloudops-single-step-vault-aws.yaml
 fi
 
 # setup aws credentials in vault
-export AWS_ACCESS_KEY_ID=`aws configure get $AWS_PROFILE.aws_access_key_id`
-export AWS_SECRET_ACCESS_KEY=`aws configure get $AWS_PROFILE.aws_secret_access_key`
-export AWS_SESSION_TOKEN=`aws configure get $AWS_PROFILE.aws_session_token`
+if [ -n "${AWS_PROFILE}" ]; then
+  CREDS_PROCESS_VALUE=`aws configure get $AWS_PROFILE.credential_process`
+  if [ -n "$CREDS_PROCESS_VALUE" ]; then
+    # profile is using credential_process
+    # we require jq make sure it exits
+    if ! command -v jq >/dev/null 2>&1; then
+      echo "ERROR: 'jq' command could not be found"
+      exit 1
+    fi
+
+    # run credential_process - parse json from it
+    CREDS_JSON=`$CREDS_PROCESS_VALUE`
+
+    export AWS_ACCESS_KEY_ID=`echo "$CREDS_JSON" | jq -r ."AccessKeyId"`
+    export AWS_SECRET_ACCESS_KEY=`echo "$CREDS_JSON" | jq -r ."SecretAccessKey"`
+    export AWS_SESSION_TOKEN=`echo "$CREDS_JSON" | jq -r ."SessionToken"`
+  else
+    # profile isn't using credential_process, get values from profile config
+    export AWS_ACCESS_KEY_ID=`aws configure get $AWS_PROFILE.aws_access_key_id`
+    export AWS_SECRET_ACCESS_KEY=`aws configure get $AWS_PROFILE.aws_secret_access_key`
+    export AWS_SESSION_TOKEN=`aws configure get $AWS_PROFILE.aws_session_token`
+  fi
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+    echo "Error: AWS_ACCESS_KEY_ID was not set and could not be loaded from AWS_PROFILE"
+    exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "Error: AWS_SECRET_ACCESS_KEY was not set and could not be loaded from AWS_PROFILE"
+    exit 1
+fi
+
+if [ -z "$AWS_SESSION_TOKEN" ]; then
+    echo "Error: AWS_SESSION_TOKEN was not set and could not be loaded from AWS_PROFILE"
+    exit 1
+fi
 
 cat > /tmp/awsConfig << EOF
 [default]
