21ba935106b9a4035ab76a95a3bb5294056f696e
GrantHoffman <grantleehoffman@users.noreply.github.com>
2021-09-24 13:17:44 -0700

    set http version explicitly to remove http2 support (#121)
    * set http version explicitly to remove http2 support

--- service/main.go
+++ service/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"os"
@@ -87,7 +88,18 @@ func main() {
 	level.Info(logger).Log("message", "starting web service", "vault addr", env.VaultAddress, "argoAddr", env.ArgoAddress)
 
 	r := setupRouter(h)
-	err = http.ListenAndServeTLS(fmt.Sprintf(":%d", env.Port), "ssl/certificate.crt", "ssl/certificate.key", r)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", env.Port),
+		Handler: r,
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			NextProtos: []string{
+				"http/1.1",
+			},
+		},
+	}
+
+	err = srv.ListenAndServeTLS("ssl/certificate.crt", "ssl/certificate.key")
 	if err != nil {
 		level.Error(logger).Log("message", "error starting service", "error", err)
 		panic("error starting service")
