978eb59578fdbdcbb4b9aab94880fa3c21277d88
pclata <1316831+pclata@users.noreply.github.com>
2022-06-16 16:58:10 -0700

    created_at not null

--- scripts/db_migrations/000002_create_tokens_table.up.sql
+++ scripts/db_migrations/000002_create_tokens_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS tokens
 (
     token_id VARCHAR(200) NOT NULL,
-    created_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL,
     project VARCHAR(80) NOT NULL,
     CONSTRAINT tokens_pkey PRIMARY KEY (token_id),
     FOREIGN KEY (project) REFERENCES projects(project)
