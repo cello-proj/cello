7b6655ac666a0c1d0deb14907be2b6a82aca6419
Yoni Smolyar <36896203+ysmolyar@users.noreply.github.com>
2022-08-15 10:01:26 -0700

    update createdbtables.sql (#352)

--- scripts/createdbtables.sql
+++ scripts/createdbtables.sql
@@ -5,4 +5,14 @@ CREATE TABLE IF NOT EXISTS projects
     repository character varying(200),
     CONSTRAINT projects_pkey PRIMARY KEY (project)
 );
+CREATE TABLE IF NOT EXISTS tokens
+(
+    token_id VARCHAR(200) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    expires_at TIMESTAMPTZ NOT NULL,
+    project VARCHAR(80) NOT NULL,
+    CONSTRAINT tokens_pkey PRIMARY KEY (token_id),
+    FOREIGN KEY (project) REFERENCES projects(project) on delete cascade on update cascade
+);
+GRANT ALL PRIVILEGES ON tokens TO cello;
 GRANT ALL PRIVILEGES ON projects TO cello;
